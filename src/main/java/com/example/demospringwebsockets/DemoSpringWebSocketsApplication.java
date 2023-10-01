451c4b6335275378c85b482fed8238812c001442
Jordy Rodriguez <jordyan1996@gmail.com>
2023-10-01 15:30:14 -0500

    Initial commit

---
+++ src/main/java/com/example/demospringwebsockets/DemoSpringWebSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringWebSocketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringwebsockets/DemoSpringWebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringwebsockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
